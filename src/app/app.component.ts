3a457e60c02a707dec0807213da1789e79e1e302
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-03-21 14:50:42 -0500

    Badges - Notificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularMaterial';
+
+  notificaciones= 2;
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
-  MatIconModule
+  MatIconModule,
+  MatBadgeModule
 ]
 
 @NgModule({
@@ -14,4 +16,8 @@ const MaterialComponents = [
   imports: [MaterialComponents],
   exports: [MaterialComponents]
 })
-export class MaterialModule { }
+export class MaterialModule {
+
+  
+}
+
