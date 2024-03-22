25fff302325f12ea8b4bc6559314f4387b9fe656
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-03-22 11:15:49 -0500

    Input - Campos de entrada

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,8 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 const MaterialComponents = [
   MatButtonModule,//Botones
@@ -31,6 +33,8 @@ const MaterialComponents = [
   MatCardModule,//Tarjetas
   MatTabsModule,//Pestañas
   MatStepperModule,//Descripción de un paso a paso
+  MatFormFieldModule,//Construir formularios
+  MatInputModule,//Construir campos de entrada
 ]
 
 @NgModule({
